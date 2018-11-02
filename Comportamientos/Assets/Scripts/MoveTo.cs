d0041c3e582faf1b0319020fbd4f3f1a3d2f75c0
pabloski300 <pabloski300@hotmail.com>
2018-11-02 11:50:44 +0100

    Commit inicial con escenario isometrico para pruebas un agente cliente que no puede entrar en la cocina.
    Se controla clicando en el escenario

---
+++ Comportamientos/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour {
+
+    NavMeshAgent agent;
+    Camera cam;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        cam = FindObjectOfType<Camera>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask(new string[] { "Barra", "Cocina", "Mesas", "Calle" })))
+            {
+                Debug.Log(hit.point);
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
---
+++ Comportamientos/Assets/Scripts/RotateAround.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.RotateAround(Vector3.zero, Vector3.up, 45 * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.RotateAround(Vector3.zero, Vector3.up, -45 * Time.deltaTime);
+        }
+    }
+}

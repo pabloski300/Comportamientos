90424e319fff69cd9624cc4ec4db0b6230e67996
César Romero Albertos <43405217+CesarRomeroAlbertos@users.noreply.github.com>
2018-11-02 13:34:39 +0100

    Añadidas clases abstractas para agentes y objetos interactivos
    He añadido clases abstractas para los agentes en los que vamos a aplicar los comportamientos y los objetos sobre los que estos agentes actuarán.

---
+++ Comportamientos/Assets/Scripts/AbstractClasses/InteractableObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.AbstractClasses
+{
+    public abstract class InteractableObject : MonoBehaviour
+    {
+        //Lista de agentes suscritos a este objeto
+        protected abstract List<StandardAgent> Agents { get; set; }
+
+        /// <summary>
+        /// Este método avisará a los agentes cuando sea necesario
+        /// </summary>
+        protected abstract void NotifyAgents();
+
+        /// <summary>
+        /// Este método se usa para añadir agentes a la lista de agentes a los que debe notificar este objeto
+        /// </summary>
+        /// <param name="Agent"></param>
+        public abstract void AddAgent(StandardAgent Agent);
+
+    }
+}
---
+++ Comportamientos/Assets/Scripts/AbstractClasses/StandardAgent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.AbstractClasses
+{
+    public abstract class StandardAgent : MonoBehaviour
+    {
+        /// <summary>
+        /// Esta máquina de estados tiene el comportamiento del agente
+        /// </summary>
+        public abstract void Behave();
+
+        /// <summary>
+        /// Este método se usará para enviar notificaciones al agente
+        /// </summary>
+        /// <param name="notification">
+        /// String que le llegará al agente y que interpretará usando diccionarios
+        /// </param>
+        public abstract void Notify(string notification);
+    }
+}

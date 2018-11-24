2c854b1a2f92c66ea0198cacedbbe0ec08f80306
pabloski300 <pabloski300@hotmail.com>
2018-11-24 19:54:28 +0100

    Intento de mascara

---
+++ Comportamientos/Assets/SetRenderQueue.cs
@@ -0,0 +1,30 @@
+﻿/*
+	SetRenderQueue.cs
+ 
+	Sets the RenderQueue of an object's materials on Awake. This will instance
+	the materials, so the script won't interfere with other renderers that
+	reference the same materials.
+*/
+
+using UnityEngine;
+
+[AddComponentMenu("Rendering/SetRenderQueue")]
+
+public class SetRenderQueue : MonoBehaviour
+{
+
+    [SerializeField]
+    protected int[] m_queues = new int[] { 3000 };
+
+    MeshRenderer renderer;
+
+    protected void Awake()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        Material[] materials = renderer.materials;
+        for (int i = 0; i < materials.Length && i < m_queues.Length; ++i)
+        {
+            materials[i].renderQueue = m_queues[i];
+        }
+    }
+}
---
+++ Comportamientos/Assets/Stencil.shader
@@ -0,0 +1,17 @@
+﻿Shader "Custom/Stencil" {
+	SubShader{
+		Tags{ "Queue" = "Geometry-1" }  // Write to the stencil buffer before drawing any geometry to the screen
+		ColorMask 0 // Don't write to any colour channels
+		ZWrite Off // Don't write to the Depth buffer
+		Pass{
+			Stencil
+			{
+				Ref 1
+				Comp Always
+				Pass Replace
+			}
+		}
+	}
+}
+	
+
